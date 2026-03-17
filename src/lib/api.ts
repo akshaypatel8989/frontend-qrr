edaa58d7e54cc94ca078860af8d6026b3eefd783
akshaypatel8989 <akshaypatel10.g@gmail.com>
2026-03-17 18:19:40 +0530

    remove

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
-const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
+//const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE= "https://emergency-qr-oejs.onrender.com"
 const headers = () => {
   const token = localStorage.getItem("token");
   return { "Content-Type": "application/json", ...(token ? { Authorization: `Bearer ${token}` } : {}) };
